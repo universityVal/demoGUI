67c3d068fc573be5b368dff79f2105097ad89b93
Alex <o.lazoriak-stud343s@chnu.edu.ua>
2019-08-13 11:12:19 +0200

    simple GUI

--- src/main/java/gui/example/demo/ControllerHello.java
+++ src/main/java/gui/example/demo/ControllerHello.java
@@ -2,14 +2,17 @@ package gui.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ControllerHello {
-    @RequestMapping("/hello")
-    String sayHello()
-    {
-        return "Hello";
+    @RequestMapping(value = {"/","index"},method = RequestMethod.GET)
+    String sayHello(Model model)
+    {   String name="wWWWW";
+        model.addAttribute("name",name);
+        return "index";
     }
     @RequestMapping("/integer")
     Integer showNumber()
